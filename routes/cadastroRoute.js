522ced996b90acfd132a92598c3a9e02588199c4
Rodrigo froes <craftland262@gmail.com>
2024-06-17 09:23:03 -0300

    Feat: Atualização Route

--- routes/cadastroRoute.js
+++ routes/cadastroRoute.js
@@ -9,7 +9,7 @@ const auth = new AuthMiddleware();
 app.post('/register', auth.veficarUsuario,  crtl.cadastroPDS); // 
 app.get('/consulta/:id', auth.veficarUsuario,  crtl.consultaId); //
 app.post('/delete/:id', auth.veficarUsuario,  crtl.deletePDS); //
-app.post('/alteracao/:id', auth.veficarUsuario,  crtl.alteracaoPDS); // 
+app.post('/alteracao', auth.veficarUsuario,  crtl.alteracaoPDS); // 
 app.post('/atividade', auth.veficarUsuario,  crtl.atividade); //
 app.post('/movimentacao', auth.veficarUsuario,  crtl.movimentacao); //
 app.get('/atividade/:id', auth.veficarUsuario,  crtl.deleteAtividade); //
